012a5bd9f5e870d1d5c1cfc497dde42e9028c085
madishak <33802709+madishak@users.noreply.github.com>
2019-05-31 18:12:28 +0300

    Added test files

--- app.js
+++ app.js
@@ -1,73 +1,16 @@
-const getArray = () => {
-    let nums = document.getElementById('input').value;
-    return nums.split(" ");
-}
+// let r = document.getElementById('input').value;
+import madina from './index10.js';
 
-const showInputArray = (arr) => {
-    arr = getArray();
-    return drawArray(arr)
-}
+const input = document.createElement('input');
+input.id = 'input';
+input.value = '';
+document.body.appendChild(input);
 
-const showArray = (arr) => drawArray(arr);
+const t = (r) => {
+    madina();
+    console.log('Madina' + r);
 
-const drawArray = (arr) => {
-    let lineValue = document.getElementById('line');
-    let inner = document.getElementById('inner');
+};
 
-    if (arr.length == 0) {
-        return null;
-    }
-    for (let i = 0; i < arr.length; i++) {
-        let newDiv = document.createElement('div');
-        newDiv.innerHTML = arr[i];
-        newDiv.className = "line";
-        inner.appendChild(newDiv);
-        newDiv.style.height = arr[i]+'px';
-        newDiv.style.display = 'block';
-    }
-
-    return newDiv;
-}
-
-
-const compareNumbersIncrease = (a , b) => a - b;
-const compareNumbersDecrease = (a , b) => b - a;
-
-const increaseSort = () => {
-    let arr = getArray();
-    let sortedArray = arr.sort(compareNumbersIncrease);
-    return showArray(sortedArray);
-}
-
-
-const decreaseSort = () => {
-    let arr = getArray();
-    let sortedArray = arr.sort(compareNumbersDecrease);
-    return showArray(sortedArray)
-}
-
-let inputValue = document.getElementById('input');
-inputValue.addEventListener('change',showInputArray);
-
-let increase = document.getElementById('inc');
-let res = increase.addEventListener('click',increaseSort);
-
-let decrease = document.getElementById('dec');
-res = decrease.addEventListener("click", decreaseSort);
-
-
-// const sortedArr = () => {
-//     let narr = getArray(arr);
-//     for (let i = 0; i < narr.length-1; i++)
-//         for (let j = i+1; j < narr.length; j++) {
-//             if (narr[i] > narr[j]) {
-//                 let temp = narr[i];
-//                 narr[i] = narr[j];
-//                 narr[j] = temp;
-//                 return narr
-//
-//             }
-//
-//         }
-//
-// }
+// let inputShow = document.getElementById('input');
+input.addEventListener('change', () => t(input.value));
